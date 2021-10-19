266307ad872d75230e97793772d5139ced3b1fbe
sifelkateb <sifelkateb@gmail.com>
2021-10-20 01:53:28 +0200

    update serverside

--- src/server/server.js
+++ src/server/server.js
@@ -21,40 +21,51 @@ const listening=()=>{
   console.log("initialize server:\n")
   console.log(`server running on port ${port}`);
 }
-const server= app.listen(port,listening);
+// const server= app.listen(port,listening);
 
-
-
-
-
-
-
-// /*
-// create a post route /sendurl
-// 1-the post route get the object sent from the client side 
-// 2- call the postData function with the articleUrl property
-// 3- get the response object from postData and send it back to the user
-// */
-// app.post('/sendurl',async(req,res)=>{
-//   console.log('new request');
-//   console.log(req.body);
-//   const response=await postData(req.body.articleUrl);
-//   res.send(response);
-// });
-
-// /*
-// perform get request to the api endpoint with the key,lang,url 
-// fetch the response data and return agreement,subjectivity,irony,confidence,score_tag properties
-// */
-// const postData=async (url)=>{
-//   try{
-//   const apiUrl=`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.key}&lang=en&url=${url}`;
-//   const response = await axios.get(apiUrl);
-//     const {agreement,subjectivity,irony,confidence,score_tag}= response.data;
-//     console.log({agreement,subjectivity,irony,confidence,score_tag});
-//     return {agreement,subjectivity,irony,confidence,score_tag};
-//   }
-//   catch(error){
-//     console.log(error);
-//   }
-// }
+const getCoordinates=async(input)=>{
+  const url=`http://api.geonames.org/searchJSON?q=${input}&maxRows=1&username=${process.env.geonames}`
+try{
+  const response = await axios.get(url);
+  console.log(response.data);
+}
+catch(error)
+{
+  console.log(error);
+}
+}
+const getCurrentWeather=async(lon,lat)=>{
+  const url=`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+  try{
+    const response= await axios.get(url);
+    console.log(response.data.data[0]);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+const getFutureForecast=async(lon,lat)=>{
+  const url=`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+  try{
+    const response= await axios.get(url);
+    console.log(response.data);
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
+}
+const getImage=async(input)=>{
+  const url=`https://pixabay.com/api/?key=${process.env.pixabay}&q=${input}&image_type=photo&safesearch=true`;
+  try{
+  const response = await axios.get(url);
+  console.log(response.data.hits[0].largeImageURL);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+// getCurrentWeather(30.46823,31.03408);
+//  getCoordinates('shubrakit');
+// getFutureForecast(30.46823,31.03408);
+// getImage('london');
