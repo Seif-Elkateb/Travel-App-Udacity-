e039734bce80cc3b95bbefd97e75151534c748a9
sifelkateb <sifelkateb@gmail.com>
2021-10-20 08:57:13 +0200

    update api functionality

--- src/server/server.js
+++ src/server/server.js
@@ -1,71 +1,83 @@
-const express= require('express');
-const axios = require('axios');
-const path=require('path');
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const port = 8081;
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Cors for cross origin allowance
-const cors = require('cors');
-const { json } = require('body-parser');
+const cors = require("cors");
+const { json } = require("body-parser");
 app.use(cors());
-app.use(express.static('dist'));
-// creating the server 
-const listening=()=>{
-  console.log("initialize server:\n")
+app.use(express.static("dist"));
+// creating the server
+const listening = () => {
+  console.log("initialize server:\n");
   console.log(`server running on port ${port}`);
-}
+};
 // const server= app.listen(port,listening);
 
-const getCoordinates=async(input)=>{
-  const url=`http://api.geonames.org/searchJSON?q=${input}&maxRows=1&username=${process.env.geonames}`
-try{
-  const response = await axios.get(url);
-  console.log(response.data);
-}
-catch(error)
-{
-  console.log(error);
-}
-}
-const getCurrentWeather=async(lon,lat)=>{
-  const url=`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
-  try{
-    const response= await axios.get(url);
-    console.log(response.data.data[0]);
-  }
-  catch(error){
+const getCoordinates = async (location) => {
+  const url = `http://api.geonames.org/searchJSON?q=${location}&maxRows=1&username=${process.env.geonames}`;
+  try {
+    const response = await axios.get(url);
+    return response.data.geonames[0];
+  } catch (error) {
     console.log(error);
   }
-}
-const getFutureForecast=async(lon,lat)=>{
-  const url=`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+};
+const getWeather = async (lon, lat, future) => {
+  let url;
+  if (future)
+    url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+  else
+    url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+  try {
+    const response = await axios.get(url);
+    return response.data.data[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getImage = async (input,backup='') => {
+  const url = `https://pixabay.com/api/?key=${process.env.pixabay}&q=${input}&image_type=photo&safesearch=true&category=places`;
+  try {
+    const response = await axios.get(url);
+    if (response.data.hits.length === 0) {
+      getImage(backup);
+    }
+    else 
+    {
+       console.log(response.data.hits[0].largeImageURL);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getData=async(location,future)=>{
   try{
-    const response= await axios.get(url);
-    console.log(response.data);
+  const {lat,lng,population,countryName}= await getCoordinates(location);
+  const  {temp,weather:{description,icon},wind_spd:windSpeed,clouds,rh:humidity}=await getWeather(lng,lat,future);
+  const weatherImg=`https://www.weatherbit.io/static/img/icons/${icon}.png`;
+  const locationImg=getImage(location,countryName);
+  return {countryName,population,temp,description,weatherImg,locationImg};
+  
   }
   catch(error)
   {
     console.log(error);
   }
 }
-const getImage=async(input)=>{
-  const url=`https://pixabay.com/api/?key=${process.env.pixabay}&q=${input}&image_type=photo&safesearch=true`;
-  try{
-  const response = await axios.get(url);
-  console.log(response.data.hits[0].largeImageURL);
-  }
-  catch(error){
-    console.log(error);
-  }
-}
-// getCurrentWeather(30.46823,31.03408);
-//  getCoordinates('shubrakit');
+//  getCurrentWeather(30.46823,31.03408);
+// getCoordinates('xx');
 // getFutureForecast(30.46823,31.03408);
 // getImage('london');
+// getData('cairo',0);
+getImage("berlin", "united kingdom");
