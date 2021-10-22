fef4087e1f443a0d2fedee11d8b7b8c18c151ec4
sifelkateb <sifelkateb@gmail.com>
2021-10-22 03:08:38 +0200

    fix bug with pixabay backup country

--- src/server/server.js
+++ src/server/server.js
@@ -25,21 +25,24 @@ const listening = () => {
 };
 const server= app.listen(port,listening);
 
+//get the coordinates from geonames api depending on the location provided 
 const getCoordinates = async (location) => {
   const url = `http://api.geonames.org/searchJSON?q=${location}&maxRows=1&username=${process.env.geonames}`;
   try {
     const response = await axios.get(url);
-    return response.data.geonames[0];
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 };
+// getweather take long,lat returned from getCoordinates function
+// getWeather also takes future bolean which indicates whether the trip after 7 days or more !
 const getWeather = async (lon, lat, future) => {
   let url;
   if (future)
-    url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+    url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`; // set url to forcast endpoint
   else
-    url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;
+    url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.weatherbit}`;  // set url to daily weather endpoint
   try {
     const response = await axios.get(url);
     return response.data.data[0];
@@ -48,16 +51,20 @@ const getWeather = async (lon, lat, future) => {
   }
 };
 
+//getImage tak two arguments input which is the name on the location and back up which is the country of that location
+// if after fetch there is no results for the location getImage call itself recursively but with the backup as input (country) 
 const getImage = async (input,backup='') => {
   const url = `https://pixabay.com/api/?key=${process.env.pixabay}&q=${input}&image_type=photo&safesearch=true&category=places`;
   try {
     const response = await axios.get(url);
+    // check if there is a result
     if (response.data.hits.length === 0) {
-      getImage(backup);
+      console.log(1);
+      return getImage(backup); // call the getImage with the country instead
     }
     else 
     {
-       return response.data.hits[0].largeImageURL
+       return response.data.hits[0].largeImageURL //
     }
   } catch (error) {
     console.log(error);
@@ -65,11 +72,22 @@ const getImage = async (input,backup='') => {
 };
 const getData=async(location,future)=>{
   try{
-  const {lat,lng,population,countryName}= await getCoordinates(location);
+  const locationResponse= await getCoordinates(location); 
+  // check if the there is a result to the location provided 
+  // if there is no result return result = none 
+  if(locationResponse.totalResultsCount===0){
+    return {result:'none'};
+  }
+  // if there is result return the location data 
+  const {lat,lng,population,countryName,name:locationName}= locationResponse.geonames[0];
+  // get the weather data fom getWeather
   const  {temp,weather:{description,icon},wind_spd:windSpeed,clouds,rh:humidity}=await getWeather(lng,lat,future);
+  // put the icon fetched from the api in url to get the corresponding png to that icon
   const weatherImg=`https://www.weatherbit.io/static/img/icons/${icon}.png`;
+  // return the img of the location 
   const locationImg=await getImage(location,countryName);
-  return {countryName,population,temp,description,weatherImg,locationImg,windSpeed,clouds,humidity};
+  console.log(locationImg)
+  return {countryName,population,temp,description,weatherImg,locationImg,windSpeed,clouds,humidity,locationName};
   }
   catch(error)
   {
@@ -77,7 +95,8 @@ const getData=async(location,future)=>{
   }
 }
 app.post('/senddata',async(req,res)=>{
-    const userInput=req.body;
-    const traveData=await getData(userInput.location,userInput.future);
+    console.log('new request to /senddata route\n',req.body)
+    const userInput=req.body; // get body of the request 
+    const traveData=await getData(userInput.location,userInput.future); // get travel data 
     res.send(traveData);
 })
